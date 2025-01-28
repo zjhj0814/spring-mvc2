8fe1b5ab51f046122273024e3d8c49d09f298155
zjhj0814 <zjhj0814@gmail.com>
2025-01-28 16:02:36 +0900

    2025.01.28
    thymleaf 프로젝트 생성

---
+++ thymelef-basic/src/main/java/hello/thymelef/ThymelefBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.thymelef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymelefBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymelefBasicApplication.class, args);
+	}
+
+}
