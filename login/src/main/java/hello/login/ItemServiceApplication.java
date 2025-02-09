f57c18c00f928ac7d34daeb875e8eb75faeec408
zjhj0814 <zjhj0814@gmail.com>
2025-02-09 23:30:58 +0900

    2025.02.09
    validation 프로젝트 종료 및 login 프로젝트 시작

--- login/src/main/java/hello/login/ItemServiceApplication.java
+++ login/src/main/java/hello/login/ItemServiceApplication.java
@@ -1,12 +1,10 @@
-package hello.itemservice;
+package hello.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Scanner;
-
 @SpringBootApplication
-public class ItemServiceApplication{
+public class ItemServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -3,11 +3,18 @@ package hello.itemservice.domain.item;
 import lombok.Data;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력해주세요.") 권장 X
 public class Item {
 
+    //@NotNull(groups = UpdateCheck.class) //수정 요구사항
     private Long id;
+    //@NotBlank(groups = {SaveCheck.class, UpdateCheck.class}) //(message="공백X")
     private String itemName;
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Max(value = 9999, groups = SaveCheck.class) //수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
